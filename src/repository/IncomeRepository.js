96c1b5b7ba317f24017f029f35ff94d14517ee0d
David Campos <fernandesc.david@gmail.com>
2022-02-02 20:35:50 -0300

    solução desafio

--- src/repository/IncomeRepository.js
+++ src/repository/IncomeRepository.js
@@ -4,13 +4,24 @@ const API_BASE_URL = 'http://localhost:3000';
 
 class IncomeRepository {
   async makeRequest(url) {
-    // @TODO: Implement method
-    return null;
+    const chunks = [];
+    return new Promise((resolve, reject) => {
+      http.get(url, response => {
+        response.on('data', data => {
+          chunks.push(data);
+        });
+        response.on('error', reject);
+        response.on('end', () => {
+          const data = Buffer.concat(chunks);
+          resolve(JSON.parse(data));
+        });
+      });
+    });
   }
 
   async getConversions() {
-    // @TODO: Implement method
-    return null;
+    const data = await this.makeRequest(`${API_BASE_URL}/convert`);
+    return data.results;
   }
 }
 
